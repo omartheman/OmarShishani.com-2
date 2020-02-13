837a5260e01d95b082627ecd721f46beeae48ae3
crazy-hobo <omarshishanischool@gmail.com>
2020-02-13 13:33:25 -0800

    Fix OS Rainbow

--- omar-shishani-2.js
+++ omar-shishani-2.js
@@ -1,32 +1,35 @@
 let OS_on;
-function runslide() {
+function runslide_O() {
     if (OS_on === true) {
-      $('.OS-S').fadeIn(1500).fadeOut(1500,
+      $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500,
       function() {
-        runslide();
+        runslide_O();
 	    });
-      $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500, function() {
-        runslide();
-	    });
+	  }
+    else {
+    	$('.OS-S').fadeIn(1500);
+    	$('.OS-O').fadeIn(1500);
+    	return;
     }
+};
+function runslide_S() {
+    if (OS_on === true) {
+      $('.OS-S').fadeOut(1500).fadeIn(1500,
+      function() {
+        runslide_S();
+	    });
+	  }
     else {
     	$('.OS-S').fadeIn(1500).delay(0);
     	$('.OS-O').fadeIn(1500).delay(0);
     	return;
     }
 };
-
 $('.OS_css_container_div').on('mouseenter', function() {
 	OS_on = true;
-	runslide();
+	runslide_O();
+	runslide_S();
 });
 $('.OS_css_container_div').on('mouseleave', function() {
 	OS_on = false;
 });
-
-// $('.OS-S').hover(function() {
-//     $(this).fadeTo("slow", 0);
-//     $(this).fadeTo("slow", 1);
-// 	}, 
-
-// );
