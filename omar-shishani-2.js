08c945c67fc2e9be3c49b9955b97c20923e6a0f2
crazy-hobo <omarshishanischool@gmail.com>
2020-02-13 11:59:26 -0800

    Fix OS fade almost

--- omar-shishani-2.js
+++ omar-shishani-2.js
@@ -1,27 +1,27 @@
 let OS_on;
 function runslide() {
     if (OS_on === true) {
-      $('.OS-S').fadeIn(1500).fadeOut(1500, function() {
-          runslide();
+      $('.OS-S').fadeIn(1500).fadeOut(1500,
+      function() {
+        runslide();
 	    });
-      $('.OS-O').delay(1500).fadeOut(1500).delay(1000).fadeIn(1500, function() {
-          runslide();
+      $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500, function() {
+        runslide();
 	    });
     }
     else {
     	$('.OS-S').fadeIn(1500).delay(0);
+    	$('.OS-O').fadeIn(1500).delay(0);
     	return;
     }
 };
 
-$('.OS-S').on('mouseenter',function() {
+$('.OS_css_container_div').on('mouseenter', function() {
 	OS_on = true;
-	console.log(true);
 	runslide();
 });
-$('.OS-S').on('mouseleave', function(){
+$('.OS_css_container_div').on('mouseleave', function() {
 	OS_on = false;
-	console.log(OS_on)
 });
 
 // $('.OS-S').hover(function() {
