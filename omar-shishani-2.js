7b168ad376da3d044b37778784a25bdf7e6c63c0
crazy-hobo <omarshishanischool@gmail.com>
2020-02-13 13:59:10 -0800

    Update runslide

--- omar-shishani-2.js
+++ omar-shishani-2.js
@@ -1,37 +1,12 @@
 let OS_on;
 function runslide_O() {
-    if (OS_on === true) {
-      $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500,
-      function() {
-        runslide_O();
-	    });
-	  }
-    else {
-    	$('.OS-S').fadeIn(1500);
-    	$('.OS-O').fadeIn(1500);
-    	return;
-    }
+  $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500);
 };
 function runslide_S() {
-    if (OS_on === true) {
-      $('.OS-S').fadeOut(1500).fadeIn(1500,
-      function() {
-        runslide_S();
-	    });
-	  }
-    else {
-    	$('.OS-S').fadeIn(1500).delay(0);
-    	$('.OS-O').fadeIn(1500).delay(0);
-    	return;
-    }
+  $('.OS-S').fadeOut(1500).fadeIn(1500);
 };
 $('.OS_css_container_div').on('mouseenter', function() {
-	OS_on = true;
-	runslide_O();
 	runslide_S();
-});
-$('.OS_css_container_div').on('mouseleave', function() {
-	OS_on = false;
 });
 
 document.onload = runslide_O_load(), runslide_S_load();
