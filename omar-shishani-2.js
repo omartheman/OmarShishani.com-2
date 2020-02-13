74d6d0a70b1c6caa6da888219c6d2c7d53485639
crazy-hobo <omarshishanischool@gmail.com>
2020-02-13 11:19:57 -0800

    Update OS animation

--- omar-shishani-2.js
+++ omar-shishani-2.js
@@ -1,6 +1,32 @@
-$('.OS-S').hover(function() {
-    $(this).fadeTo("slow", 0);
-    $(this).fadeTo("slow", 1);
-	}, 
+let OS_on;
+function runslide() {
+    if (OS_on === true) {
+      $('.OS-S').fadeIn(1500).fadeOut(1500, function() {
+          runslide();
+	    });
+      $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500, function() {
+          runslide();
+	    });
+    }
+    else {
+    	$('.OS-S').fadeIn(1500).delay(0);
+    	return;
+    }
+};
 
-);
+$('.OS-S').on('mouseenter',function() {
+	OS_on = true;
+	console.log(true);
+	runslide();
+});
+$('.OS-S').on('mouseleave', function(){
+	OS_on = false;
+	console.log(OS_on)
+});
+
+// $('.OS-S').hover(function() {
+//     $(this).fadeTo("slow", 0);
+//     $(this).fadeTo("slow", 1);
+// 	}, 
+
+// );
