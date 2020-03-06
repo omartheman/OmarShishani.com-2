96d1d370ad4381405a43183303fb896f7cbc44d3
Omar Shishani <omarshishanischool@gmail.com>
2020-03-06 11:18:49 -0800

    Remove os-on, change os-o to o-plain & s

--- omar-shishani-2.js
+++ omar-shishani-2.js
@@ -1,13 +1,12 @@
-let OS_on;
 let fadeCompleted = false;
 function runslide_O() {
-  $('.OS-O').delay(1500).fadeOut(1500).fadeIn(1500, function(){
+  $('.o-plain').delay(1500).fadeOut(1500).fadeIn(1500, function(){
   	fadeCompleted = true; 
   });
 };
 function runslide_S() {
 	fadeCompleted = false;
-  $('.OS-S').fadeOut(1500).fadeIn(1500);
+  $('.s-plain').fadeOut(1500).fadeIn(1500);
 };
 function runslide_both() {
 	if (fadeCompleted === true) {
@@ -23,10 +22,10 @@ $('.OS_css_container_div').on('click', runslide_both);
 
 document.onload = runslide_O_load(), runslide_S_load();
 function runslide_O_load() {
-  $('.OS-O').fadeIn(2000);
+  $('.o-plain').fadeIn(2000);
 };
 function runslide_S_load() {
-  $('.OS-S').delay(1500).fadeIn(2000, function(){
+  $('.s-plain').delay(1500).fadeIn(2000, function(){
   	fadeCompleted = true;
   });
 };
