7578fd1ffff42a101ea992b9584a44d5bf76fe4e
Omar Shishani <omarshishanischool@gmail.com>
2020-03-20 23:32:31 -0700

    Change url, discover cpanel doesnt support mongo

--- things_i_do/app.js
+++ things_i_do/app.js
@@ -4,19 +4,43 @@ mongoose           = require('mongoose'),
 app                = express();
 
 //APP CONFIG
-mongoose.connect('mongodb://localhost/omar_shishani_database');
+mongoose.connect('mongodb://localhost:27017/database_omar', { useUnifiedTopology: true, useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
 //MONGOOSE/MODEL CONFIG
 const articleSchema = new mongoose.Schema({
+  _id: Number,
+  name: String,
+  url: String,
   title: String,
   image: String, 
   body: String, 
   created: {type: Date, default: Date.now},
 });
 const Article = mongoose.model('Article', articleSchema);
+
+// //Adding new article
+// let smooth_animations = new Article({
+//   _id: '1',
+//   name: 'CSS Smooth Animations',
+//   url: 'https://medium.com/@omarshishanischool/css-animations-smooth-change-from-100-to-0-b578bc33a643',
+//   title: 'CSS Animations: Smooth change from 100% to 0%',
+//   image: '',
+//   body: '',
+// });
+
+// smooth_animations.save(function(err, article){
+//   if (err) {
+//     console.log('something went wrong')
+//     console.log(err);
+//   } else {
+//     console.log('we just saved an article to the db');
+//     console.log(article);
+//   }
+// });
+
 
 app.get('/', function(req, res){
   res.render('index');
@@ -27,7 +51,14 @@ app.get("/things_i_do", function(req, res){
 });
 
 app.get("/articles", function(req, res){
-  res.render('articles.ejs');
+  Article.find({}, function(err, articles){
+    if(err){
+      console.log('Error!');
+      console.log(err);
+    } else {
+      res.render('articles', {articles: articles});
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
