4a7cda37205bab256cc1de6a4a69f041d1c50c69
Omar Shishani <omarshishanischool@gmail.com>
2020-03-21 00:18:05 -0700

    Add articles object to ejs

--- things_i_do/app.js
+++ things_i_do/app.js
@@ -3,23 +3,30 @@ express            = require('express'),
 mongoose           = require('mongoose'),
 app                = express();
 
-//APP CONFIG
-mongoose.connect('mongodb://localhost:27017/database_omar', { useUnifiedTopology: true, useNewUrlParser: true });
+const articles = [
+  { id: '001',
+    name: 'CSS Smooth Transitions',
+    url: 'https://medium.com/@omarshishanischool/css-animations-smooth-change-from-100-to-0-b578bc33a643'
+  }
+];
+
+// //APP CONFIG
+// mongoose.connect('mongodb://localhost:27017/database_omar', { useUnifiedTopology: true, useNewUrlParser: true });
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//MONGOOSE/MODEL CONFIG
-const articleSchema = new mongoose.Schema({
-  _id: Number,
-  name: String,
-  url: String,
-  title: String,
-  image: String, 
-  body: String, 
-  created: {type: Date, default: Date.now},
-});
-const Article = mongoose.model('Article', articleSchema);
+// //MONGOOSE/MODEL CONFIG
+// const articleSchema = new mongoose.Schema({
+//   _id: Number,
+//   name: String,
+//   url: String,
+//   title: String,
+//   image: String, 
+//   body: String, 
+//   created: {type: Date, default: Date.now},
+// });
+// const Article = mongoose.model('Article', articleSchema);
 
 // //Adding new article
 // let smooth_animations = new Article({
@@ -51,14 +58,7 @@ app.get("/things_i_do", function(req, res){
 });
 
 app.get("/articles", function(req, res){
-  Article.find({}, function(err, articles){
-    if(err){
-      console.log('Error!');
-      console.log(err);
-    } else {
-      res.render('articles', {articles: articles});
-    }
-  });
+  res.render('articles', {articles: articles});
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
