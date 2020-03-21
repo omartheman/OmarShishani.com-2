d3ea7b0d45f5c2fa1010aa560313b6ee662d6f8b
Omar Shishani <omarshishanischool@gmail.com>
2020-03-21 14:52:57 -0700

    index.js connected

--- things_i_do/app.js
+++ things_i_do/app.js
@@ -1,22 +1,66 @@
-const bodyParser   = require('body-parser'),
-express            = require('express'),
-mongoose           = require('mongoose'),
-app                = express();
+const Express = require("express");
+const BodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const mongoose = require('mongoose');
 
-const articles = [
-  { id: '001',
-    name: 'CSS Smooth Transitions',
-    url: 'https://medium.com/@omarshishanischool/css-animations-smooth-change-from-100-to-0-b578bc33a643'
-  }
-];
+const CONNECTION_URL = "mongodb+srv://omarnaod:3yeDroplets@cluster0-zzysp.mongodb.net/test?retryWrites=true&w=majority";
+const DATABASE_NAME = "articles";
+
+var app = Express();
+
+var uri = 'mongodb+srv://omarnaod:3yeDroplets@cluster0-zzysp.mongodb.net/test?retryWrites=true&w=majority';
+
+var db = mongoose.connect(uri).catch((error) => { console.log(error); });
+
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({ extended: true }));
+
+var database, collection;
+
+app.listen(3000, () => {
+    MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true }, (error, client) => {
+        if(error) {
+            throw error;
+        }
+        database = client.db(DATABASE_NAME);
+        collection = database.collection("people");
+        console.log("Connected to `" + DATABASE_NAME + "`!");
+    });
+});
+
+app.post("/person", (request, response) => {
+  collection.insert(request.body, (error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result.result);
+  });
+});
+
+app.get("/people", (request, response) => {
+  collection.find({}).toArray((error, result) => {
+      if(error) {
+          return response.status(500).send(error);
+      }
+      response.send(result);
+  });
+});
+
+// const articles = [
+//   { id: '001',
+//     name: 'CSS Smooth Transitions',
+//     url: 'https://medium.com/@omarshishanischool/css-animations-smooth-change-from-100-to-0-b578bc33a643'
+//   }
+// ];
 
 // //APP CONFIG
-// mongoose.connect('mongodb://localhost:27017/database_omar', { useUnifiedTopology: true, useNewUrlParser: true });
+// mongoose.connect('mongodb+srv://omarnaod:3yeDroplets%21@cluster0-zzysp.mongodb.net/test?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true });
 app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(Express.static('public'));
+app.use(BodyParser.urlencoded({extended: true}));
 
-// //MONGOOSE/MODEL CONFIG
+//MONGOOSE/MODEL CONFIG
 // const articleSchema = new mongoose.Schema({
 //   _id: Number,
 //   name: String,
@@ -28,7 +72,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // });
 // const Article = mongoose.model('Article', articleSchema);
 
-// //Adding new article
+//Adding new article
 // let smooth_animations = new Article({
 //   _id: '1',
 //   name: 'CSS Smooth Animations',
@@ -61,6 +105,6 @@ app.get("/articles", function(req, res){
   res.render('articles', {articles: articles});
 });
 
-app.listen(process.env.PORT || 3000, process.env.IP, function(){
+app.listen(process.env.PORT || 5000, process.env.IP, function(){
   console.log('Server is running on port 3000');
 });
