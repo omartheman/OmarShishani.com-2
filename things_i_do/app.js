ca79a1b0563fad3138c9a46e9971a36a81bfe850
Omar Shishani <omarshishanischool@gmail.com>
2020-03-22 20:14:54 -0700

    Add process.env PORT to app.listen

--- things_i_do/app.js
+++ things_i_do/app.js
@@ -53,6 +53,10 @@ app.get('/articles', (req, res) => {
   });
 });
 
-app.listen(3000, () => { 
-  console.log('Server is listening on port 3000') 
+// app.listen(3000, () => { 
+//   console.log('Server is listening on port 3000') 
+// });
+
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+  console.log('Server is running on port 3000');
 });
